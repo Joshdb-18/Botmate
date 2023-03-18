1bc8b78bdc6b697da3f208fa75581dd22ca0a80d
Joshdb-18 <joshuadami17@gmail.com>
2023-03-18 10:36:37 +0100

    tbc...

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     SECRET_KEY = "this-is-a-super-secret-key"
-    OPENAI_KEY = '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'
+    OPENAI_KEY = 'api key'
 
 config = {
     'development': DevelopmentConfig,
