df0983e5e2a664bc7cef773f21450c569f929b22
Joshdb-18 <joshuadami17@gmail.com>
2023-03-18 14:58:15 +0100

    update response

---
+++ aiapi.py
@@ -0,0 +1,21 @@
+import openai
+import config
+openai.api_key = config.DevelopmentConfig.OPENAI_KEY
+
+def generateChatResponse(prompt):
+	messages = []
+	messages.append({"role": "system", "content": "Your name is Botmate and you are a helpful assistant, you were created by Damilola Joshua Oluwafemi a student at alx, he created you as a project, and you only answer questions based on education."})
+
+	question = {}
+	question['role'] = 'user'
+	question['content'] = prompt
+	messages.append(question)
+
+	response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+	try:
+		answer = response['choices'][0]['message']['content'].replace('\n', '<br>')
+	except Exception:
+		answer = 'Oops you beat the AI, try a different question, if the problem persists, come back later.'
+
+	return answer
+
--- app.py
+++ app.py
@@ -3,15 +3,6 @@ import config
 import openai
 import aiapi
 
-openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ]
-)
 
 def page_not_found(e):
   return render_template('404.html'), 404
@@ -28,28 +19,11 @@ def index():
     if request.method == 'POST':
         prompt = request.form['prompt']
         res = {}
-        res['answer'] = generateChatResponse(prompt)
+        res['answer'] = aiapi.generateChatResponse(prompt)
         return jsonify(res), 200
 
     return render_template('index.html', **locals())
 
-def generateChatResponse(prompt):
-    messages = []
-    messages.append({"role": "system", "content": "Your name is Karabo. You are a helpful assistant."})
-
-    question = {}
-    question['role'] = 'user'
-    question['content'] = prompt
-    messages.append(question)
-
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
-
-    try:
-        answer = response['choices'][0]['message']['content'].replace('\n', '<br>')
-    except:
-        answer = 'Oops you beat the AI, try a different question, if the problem persists, come bacl later.'
-
-    return answer
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8888', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
