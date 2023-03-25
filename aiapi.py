2ba2c68ed9e72f4aba0a9f197a37592319e81159
Joshdb-18 <joshuadami17@gmail.com>
2023-03-25 13:07:58 +0100

    add the system role and generate chatbot response

--- aiapi.py
+++ aiapi.py
@@ -3,20 +3,27 @@ import openai
 
 openai.api_key = config.DevelopmentConfig.OPENAI_KEY
 
+
 def generateChatResponse(prompt):
-	messages = []
-	messages.append({"role": "system", "content": "Your name is Botmate and you are a helpful assistant, you were created by Damilola Joshua Oluwafemi a student at alx, he created you as a project, and you only answer questions based on education."})
+    messages = []
+    messages.append({"role": "system", "content": "Your name is Botmate
+                    and you are a helpful assistant, you were created by
+                    Damilola JoshuaOluwafemi a student at alx, he created
+                    you as a project, and you only answer questions based
+                    on education."})
 
-	question = {}
-	question['role'] = 'user'
-	question['content'] = prompt
-	messages.append(question)
+    question = {}
+    question['role'] = 'user'
+    question['content'] = prompt
+    messages.append(question)
 
-	response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-	try:
-		answer = response['choices'][0]['message']['content'].replace('\n', '<br>')
-	except Exception:
-		answer = 'Oops you beat the AI, try a different question, if the problem persists, come back later.'
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                            messages=messages)
+    try:
+        answer = response['choices'][0]['message']['content']
+        answer = answer.replace('\n', '<br>')
+    except Exception:
+        answer = 'Oops you beat the AI, try a different question,
+        if the problem persists, come back later.'
 
-	return answer
-
+    return answer
