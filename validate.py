5a82972fe54add5c2ed3a186f236b97ce181301f
Joshdb-18 <joshuadami17@gmail.com>
2023-03-25 13:30:30 +0100

    create database

--- validate.py
+++ validate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-from flask import Flask
 from app import db, User
+from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
